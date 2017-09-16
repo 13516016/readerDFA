0825cf900b59d3600f4497c770ad495a3d0d0725
Adylan Roaffa Ilmy <adylanrff@gmail.com>
2017-09-16 14:00:28 +0700

    done

--- readerDFA.c
+++ readerDFA.c
@@ -1,26 +1,37 @@
 #include<stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 #define maxKol 100
 #define maxBar 100
 #define maxStr 100
+#define ACCEPTED "ACCEPTED"
+#define REJECTED "REJECTED"
+#define INVALID "#INVALID#"
 
 
 typedef struct {
   int nKolom;
   int nBaris;
+  int nFinalState;
   char tab[maxBar][maxKol][maxStr];
-  char finalState[maxKol][maxStr];
+  char finalTab[maxKol][maxStr];
 
-} transTab;
+} TransTab;
 
 typedef struct {
   int nInputs;
   char tab[maxBar][maxStr];
-} inputs;
+} Inputs;
 
-void readTransTable(transTab *T){
+typedef struct {
+  int nState;
+  char * status;
+  char tab[maxBar][maxStr];
+} SessionState;
+
+void readTransTable(TransTab *T){
   // TODO : make matriks from inputs
   int i; //row
   int j; //column
@@ -32,26 +43,42 @@ void readTransTable(transTab *T){
           scanf("%s",&(*T).tab[i][j]);
         }
     }
+  printf("number of final states : ");scanf("%d",&(*T).nFinalState);
+  for (i = 0; i < (*T).nFinalState; i++) {
+    scanf("%s",&(*T).finalTab[i]);
+  }
+
 }
 
-void writeTransTable(transTab T){
+void writeTransTable(TransTab T){
   // TODO : make matriks from inputs
   int i; //row
   int j; //column
 
+
   for (i = 0; i<= (T).nBaris; i++){
     for (j = 0; j<= (T).nKolom; j++ ){
-        //Skip the 0,0 index.
         printf("%s",(T).tab[i][j]);
         if (j!=(T).nKolom){
           printf(" ");
         }
       }
+
       printf("\n" );
     }
+
+    printf("FINAL STATES\n");
+    for (i=0; i<(T).nFinalState;i++){
+        printf("%s", (T).finalTab[i]);
+      if(i!= (T).nFinalState-1){
+        printf(" ");
+      }
+    }
+
+
 }
 
-void readInputs(inputs * IN){
+void readInputs(Inputs * IN){
   int i;
 
   printf("number of inputs : ");scanf("%d",&(*IN).nInputs);
@@ -62,7 +89,7 @@ void readInputs(inputs * IN){
 
 }
 
-void writeInputs(inputs IN){
+void writeInputs(Inputs IN){
   int i;
 
   for (i = 0; i<(IN).nInputs;i++){
@@ -75,13 +102,101 @@ void writeInputs(inputs IN){
 
 }
 
-int main(){
-//variables
-  transTab T;
-  inputs IN;
+char * transFunction(char * input, char * currentState, TransTab T){
+    int i;
+    int inputIdx = -1;
+    int stateIdx = -1;
+    char * state;
 
-  readTransTable(&T);
+    for (i = 0; i <= (T).nKolom; i++) {
+        if (strcmp(input,(T).tab[0][i])==0){
+          inputIdx = i;
+        }
+    }
+
+    for (i = 0; i<= (T).nBaris;i++){
+      if (strcmp(currentState,(T).tab[i][0])==0){
+        stateIdx = i;
+      }
+    }
+
+    if (inputIdx != -1 && stateIdx != -1){
+      state = (T).tab[stateIdx][inputIdx];
+    }
+    else {
+      state = INVALID;
+    }
+
+    return state;
+}
+
+void simulateDFA(Inputs IN, TransTab T, SessionState * S){
+  int nIn=0;
+  int i;
+  int j;
+  char * currentState = (T).tab[1][0];
+  (*S).status = INVALID;
+
+  strcpy((*S).tab[nIn],currentState);
+  while ((nIn<=(IN).nInputs) && (strcmp(currentState,INVALID)!=0) ){
+    currentState = transFunction((IN).tab[nIn],currentState,T);
+    nIn++;
+    strcpy((*S).tab[nIn],currentState);
+  }
+
+  (*S).nState = nIn;
+
+
+  if (strcmp((*S).tab[nIn]-1,INVALID)!=0){
+    for (i = 0; i < (*S).nState; i++) {
+      if (strcmp((T).finalTab[i],(*S).tab[nIn])==0) {
+        (*S).status = ACCEPTED;
+        break;
+      }
+      else {
+        (*S).status = REJECTED;
+      }
+    }
+  }
+
+}
+
+void writeSessionState(SessionState S){
+  int i;
+
+  if (strcmp((S).status,REJECTED)==0){
+    printf("%s",REJECTED);
+  }
+  else if (strcmp((S).status,INVALID)==0){
+    printf("%s", INVALID);
+  }
+  else {
+    printf("%s",ACCEPTED);
+  }
+  printf("\n" );
+    for (i = 0; i < (S).nState; i++) {
+      printf("%s",(S).tab[i]);
+      if (i!= (S).nState-1){
+        printf(" --> ");
+    }
+
+  }
+
+}
+
+
+int main(){
+  TransTab T;
+  Inputs IN;
+  SessionState S;
+  //variables
+
+  readTransTable(&T);printf("\n");
   writeTransTable(T); printf("\n");
-  readInputs(&IN);
+  readInputs(&IN);printf("\n");
   writeInputs(IN); printf("\n");
+  simulateDFA(IN,T,&S);
+  writeSessionState(S);printf("\n" );
+
+  // printf("%s\n",transFunction((IN).tab[0],(T).tab[0][1],T));
 }
